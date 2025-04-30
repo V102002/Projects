84ea7b9db982699feb32c21f70b29a8ffd8b7d36
V102002 <vssnsvs@gmail.com>
2025-04-30 15:05:52 +0530

    Add code to Audio Book

---
+++ Audio_Book_Python/main.py
@@ -0,0 +1,26 @@
+import pyttsx3
+import PyPDF2
+from tkinter.filedialog import *
+
+player=pyttsx3.init() #initializing
+
+try:
+    #Reading the name of the PDF
+    book=askopenfilename()
+
+    #Initialize with PDF reader
+    pdfreader=PyPDF2.PdfReader(book)
+
+
+    #Get the number of pages from the pdf
+    pages=len(pdfreader.pages)
+
+    #Read the data from the pages
+    for num in range(pages):
+        page = pdfreader.pages[num]
+        text = page.extract_text()
+        if text:
+            player.say(text)
+        player.runAndWait()
+except Exception as e:
+    print("Error in loading PDF:",e)
