4fc7b2db89d58b6a313f480ccdac61b4ee0e3c63
V102002 <vssnsvs@gmail.com>
2025-06-10 15:48:30 +0530

    Done

--- Cool_Website/src/components/Footer.jsx
+++ Cool_Website/src/components/Footer.jsx
@@ -12,7 +12,7 @@ const Footer = () => {
     <footer className='w-screen bg-violet-300 py-4 text-black'>
       <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
         <p className='text-center text-sm font-light md:text-left'>
-          &copy; Nova 2024. All rights reserved  
+          &copy; Vinayak 2024. All rights reserved  
         </p>
         <div className='flex justify-center gap-4 md:justify-start'>
             {links.map((link)=>(
