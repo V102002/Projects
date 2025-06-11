d34df500a2c42e5e7f3567d1b8c6c1e368baeb5c
V102002 <vssnsvs@gmail.com>
2025-06-11 21:44:44 +0530

    Writing API

--- Movie_Website/react-app/src/App.jsx
+++ Movie_Website/react-app/src/App.jsx
@@ -3,9 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Search from './components/Search'
 
+// Where can the site ask for the movies
+const API_BASE_URL='https://api.themoviedb.org/3/discover/movie';
+
+// Get access to the API key
+const API_KEY= import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS={
+  method:'GET', //Get some movies
+  headers:{
+    accept:'application/json', //The API will send a JSON object 
+    Authorization: `Bearer ${API_KEY}`, //Authenticate the API, verifies who is trying to making that request
+  }
+}
+
 //The Main Component
 const App=()=>{
   const [searchTerm,setSearchTerm]=useState('');
+
+  const fetchMovies=async ()=>{
+    try{
+
+    }catch(error){
+      console.log(`Error fetching movies: ${error}`);
+    }
+  }
+
+  useEffect(()=>{
+
+  },[])
 
   return (
     <main>
