d9807d0e7ffaa7875962d0b37e458734c5b121f8
V102002 <vssnsvs@gmail.com>
2025-06-18 15:22:38 +0530

    Mortgage and Interest

--- Mortgage_Repayment_Calculator/src/components/Input.jsx
+++ Mortgage_Repayment_Calculator/src/components/Input.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Input = ({text, heading}) => {
   return (
     <div className='flex flex-col'>
-      <p className='text-sm text-left mb-2'>{heading}</p>
+      <p className='text-sm text-left mb-2 font-semibold'>{heading}</p>
       <div className='relative'>
-        <input className='border rounded left-2 w-full py-2 pl-2  ' type="number" />
-        <span className='absolute right-[1px] bottom-[0.1px] bg-slate-100 p-2 px-4 rounded-tr rounded-br font-semibold z-0 '>{text}</span>
+        <input className='font-bold border z-2 relative rounded left-0 w-full py-2 pl-2  ' type="number" />
+        <span className='absolute right-0 bottom-[0.1px] bg-slate-100 p-2 px-4 rounded-tr rounded-br font-semibold '>{text}</span>
       </div>
     </div>
   )
