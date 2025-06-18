4131e8fee590bcbd4845eb5bdbb5cf9ad62fc4b8
V102002 <vssnsvs@gmail.com>
2025-06-18 15:23:36 +0530

    2 options

--- Mortgage_Repayment_Calculator/src/components/Type.jsx
+++ Mortgage_Repayment_Calculator/src/components/Type.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Type = () => {
+const Type = ({type,selected,setSelected}) => {
   return (
     <div>
-      
+        <label className={`flex items-center px-2 py-3 mb-3 border rounded cursor-pointer 
+          ${selected === type ? 'bg-yellow-100 border-yellow-400' : 'border-gray-300'}`}>
+          <input type="radio"
+         value={type}
+         className='mx-3 scale-125 accent-yellow-200'
+         checked={selected === type}
+         onChange={()=>setSelected(type)}
+         />
+          <span className='font-semibold'>{type}</span>
+        </label>
+        
+         
     </div>
   )
 }
