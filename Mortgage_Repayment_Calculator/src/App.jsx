e20f759295f2efd7a8e2b395d0fa3c9ff8bd0a46
V102002 <vssnsvs@gmail.com>
2025-06-19 08:18:30 +0530

    minor changes, repaired it

--- Mortgage_Repayment_Calculator/src/App.jsx
+++ Mortgage_Repayment_Calculator/src/App.jsx
@@ -51,10 +51,10 @@ function App() {
 
   return (
     <main>
-      <div className='container flex rounded-3xl h-fit md: flex-col w-full'>
+      <div className='container flex rounded-3xl h-fit'>
         
         {/* LEFT FORM */}
-        <div className='Left-form w-1/2 px-10 py-6 flex flex-col items-start md: w-full text-sm'>
+        <div className='Left-form w-1/2 px-10 py-6 flex flex-col items-start'>
           <div className='w-full flex justify-between items-start'>
             <h1 className='text-left py-5 font-bold'>Mortgage Calculator</h1>
             <a
@@ -128,7 +128,7 @@ function App() {
         </div>
 
         {/* RIGHT SIDE */}
-        <div className='Right-form w-1/2 bg-slate-800 rounded-tr-3xl rounded-br-3xl rounded-bl-[80px] md: w-full text-sm'>
+        <div className='Right-form w-1/2 bg-slate-800 rounded-tr-3xl rounded-br-3xl rounded-bl-[80px]'>
           <h1 className='text-left mx-10 pt-10 font-bold text-white '>Your results</h1>
           <p className='text-sm mx-10 py-2 text-left text-slate-400'>
             Your results are shown below based on the information you provided. To adjust the results, edit the form and click on 'calculate repayments' again.
